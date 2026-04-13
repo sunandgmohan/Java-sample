f7be538420acb29c730fe424f63a80647417c64a
sunandgmohan <sunandgmohan@gmail.com>
2026-04-13 10:09:36 +0530

    Update greeting message in Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Hello, this is my sample java project!!! " + currentDate);
+        System.out.println("Hello, this is my git action java project!!! " + currentDate);
     }
 }
